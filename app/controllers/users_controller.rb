4a1942a043a723bfef8d61c581468a347878652f
Jesse Brooklyn Hannah <jesse@jbhannah.net>
2022-12-14 16:46:36 -0800

    UsersController stub

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+class UsersController < ApplicationController
+  def show; end
+
+  def new; end
+
+  def edit; end
+
+  def create; end
+
+  def update; end
+
+  def destroy; end
+end
