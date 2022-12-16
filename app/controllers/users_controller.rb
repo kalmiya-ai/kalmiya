6b3460e90a4a315c8c3cca92dfaf0badf6558fdb
Jesse Brooklyn Hannah <jesse@jbhannah.net>
2022-12-16 10:51:59 -0800

    Basic user creation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,13 +3,26 @@
 class UsersController < ApplicationController
   def show; end
 
-  def new; end
+  def new
+    @user = User.new
+  end
 
   def edit; end
 
-  def create; end
+  def create
+    @user = User.create!(create_params)
+
+    flash[:success] = t('.success')
+    redirect_to action: :new
+  end
 
   def update; end
 
   def destroy; end
+
+  private
+
+  def create_params
+    params.require(:user).permit(:name, :email, :email_confirmation, :password, :password_confirmation)
+  end
 end
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -3,7 +3,25 @@
 require 'rails_helper'
 
 RSpec.describe 'Users' do
-  describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+  subject { response }
+
+  describe 'GET /user/new' do
+    before { get '/user/new' }
+
+    it { is_expected.to have_http_status :ok }
+  end
+
+  describe 'POST /user' do
+    context 'with valid parameters' do
+      let(:user) { attributes_for(:user) }
+
+      before { post '/user', params: { user: } }
+
+      it { is_expected.to redirect_to '/user/new' }
+
+      it 'creates a user' do
+        expect(User.find_by(email: user[:email])).to have_attributes(**user.slice(:id, :name, :email))
+      end
+    end
   end
 end
